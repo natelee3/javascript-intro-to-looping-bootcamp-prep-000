8d041a6891874f9d5bd13d3f40d695e3abd4573c
Nate Lee <nleepercussion@gmail.com>
2017-03-22 14:49:09 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,34 @@
+var array = []
+
+function forLoop(array){
+  for (let i = 0; i<25; i++) {
+    if (i === 1) {
+    array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push("I am ${1} strange loops.");
+    }
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while (n >0) {
+    console.log(--n)
+  }
+  return 'done';
+}
+
+var arr = []
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (maybeTrue() && array.length > 0);
+
+  return array;
+}
